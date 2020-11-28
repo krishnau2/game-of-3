7e9b0ddf757773529ed4b84d21eda568b212bc25
krishnau2 <krishnau2@gmail.com>
2020-11-29 00:09:04 +0530

    Generating a new game from Home page with static user details

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { db } from "../services/firebase";
+
+const initialInputNumber = 19;
 
 function Home() {
+  const [gameKey, setGameKey] = useState(null);
+
+  const createGame = async () => {
+    let newGameObject = {
+      startingNumber: initialInputNumber,
+      currentNumber: initialInputNumber,
+      currentPlayer: "playerA",
+      createdBy: "playerA",
+      winner: null,
+      players: [{ name: "playerA", email: "a@a.com", type: "user" }],
+      status: "waiting",
+      log: [
+        {
+          player: "playerA",
+          inputNumber: null,
+          startingNumber: initialInputNumber,
+          divisibleByThree: null,
+          nextNumber: initialInputNumber
+        }
+      ]
+    };
+    let newGame = await db.push(newGameObject);
+    console.log("New Game Key: ", newGame.key);
+    setGameKey(newGame.key);
+  };
+
   return (
     <div className="Home">
       <h2>Home page</h2>
+      <div>
+        <button onClick={createGame}>Create New Game</button>
+      </div>
+      <div>
+        <h3>New Game Key: {gameKey}</h3>
+      </div>
     </div>
   );
 }
